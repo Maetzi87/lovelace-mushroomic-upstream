34dd8df876e494c1304ac147763eb524cb13fab3
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-03-23 16:04:49 +0100

    Update mushroom.ts

--- src/mushroom.ts
+++ src/mushroom.ts
@@ -15,7 +15,7 @@ import "./cards/media-player-card/media-player-card";
 import "./cards/number-card/number-card";
 import "./cards/person-card/person-card";
 import "./cards/select-card/select-card";
-import "./cards/template-card/template-card";
+import "./cards/template-card/resize-card";
 import "./cards/title-card/title-card";
 import "./cards/update-card/update-card";
 import "./cards/vacuum-card/vacuum-card";
