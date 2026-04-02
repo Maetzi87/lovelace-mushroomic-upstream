40148ebb29280d17fc89725f70e87304094f73e7
Maetzi87 <eva.stoiser@gmx.net>
2026-04-02 23:29:51 +0200

    Delete unused files

--- src/mushroomic.ts
+++ src/mushroomic.ts
@@ -2,8 +2,6 @@ import { version } from "../package.json";
 
 import "./cards/power-card/power-card";
 
-import "./badges/template/template-badge";
-
 console.info(
   `%c🍄 Mushroomic 🍄 - ${version}`,
   "color: #ef5350; font-weight: 700;"
