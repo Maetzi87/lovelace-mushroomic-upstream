a5d1a045eb8c359fc39df71f29c229d37f46957b
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-03 21:55:33 +0200

    Update power-card.ts

--- src/cards/power-card/power-card.ts
+++ src/cards/power-card/power-card.ts
@@ -8,7 +8,7 @@ import { unsafeSVG } from "lit/directives/unsafe-svg.js";
 import "./power-card-editor";
 import memoizeOne from "memoize-one";
 import hash from "object-hash/dist/object_hash";
-import "./utils/icons/register-mushic-icons";
+import "../../utils/icons/register-mushic-icons";
 import {
   actionHandler,
   ActionHandlerEvent,
