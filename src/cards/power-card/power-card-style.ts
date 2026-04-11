c08daaef2409e4b76c19013f02f8f2fd347ac15c
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-11 17:03:14 +0200

    Update power-card-style.ts

--- src/cards/power-card/power-card-style.ts
+++ src/cards/power-card/power-card-style.ts
@@ -20,8 +20,8 @@ export const powerCardStyles = [
         --ha-ripple-color: var(--mushic-ripple-color, var(--mushic-icon-color, var(--state-inactive-color)));
         --ha-ripple-hover-opacity: 0.04;
         --ha-ripple-pressed-opacity: 0.12;
-        min-height: var(--mushic-card-auto-height, auto);
-        height: var(--mushic-card-height, 100%);
+        min-height: var(--mushic-card-auto-height, 56px);
+        height: var(--mushic-card-height, -webkit-fill-available);
         transition:
           box-shadow 180ms ease-in-out,
           border-color 180ms ease-in-out;
