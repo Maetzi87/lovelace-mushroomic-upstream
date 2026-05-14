dea90e409b4c7b64a5de5465374960fa77b1df90
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-05-15 01:51:26 +0200

    Update power-card.ts

--- src/cards/power-card/power-card.ts
+++ src/cards/power-card/power-card.ts
@@ -3,7 +3,7 @@ import { css, html, LitElement, nothing, PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
-import { styleMap } from "lit/directives/style-map.js";
+import { styleMap } from "lit/directives/style-map.js"; 
 import { unsafeSVG } from "lit/directives/unsafe-svg.js";
 import "./power-card-editor";
 import memoizeOne from "memoize-one";
